10e65b7ffde6574304071a74b33f7c492a547a3d
James Porter <porterjamesj@gmail.com>
2014-11-14 20:17:13 -0600

    initial commit

---
+++ scripts/bootstrap.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/bash
+
+# bootstrap mesos on an ubuntu vm (in sullivan)
+
+# add the relevant ppa
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv E56151BF
+DISTRO=$(lsb_release -is | tr '[:upper:]' '[:lower:]')
+CODENAME=$(lsb_release -cs)
+echo "deb http://repos.mesosphere.io/${DISTRO} ${CODENAME} main" | \
+  sudo tee /etc/apt/sources.list.d/mesosphere.list
+sudo apt-get -y update
+
+# install
+sudo apt-get install -y mesos
