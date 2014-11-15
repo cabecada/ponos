2b9d9ec5df52c00843ea562302a80c545f8a2f7f
James Porter <porterjamesj@gmail.com>
2014-11-15 00:36:49 -0600

    wip python bindings install

--- scripts/start_master.sh
+++ scripts/start_master.sh
@@ -16,3 +16,17 @@ sudo sh -c "echo manual > /etc/init/mesos-slave.override"
 
 # start master
 sudo service mesos-master restart
+
+
+# python driver stuff shout out to
+# https://github.com/ceteri/exelixi/blob/master/bin/local_install.sh
+sudo apt-get -y install python-dev
+sudo apt-get -y install python-pip
+sudo apt-get -y install python-protobuf
+
+sudo apt-get -y install git
+
+# install mesos python bindings
+EGG=mesos-0.20.0-py2.7-linux-x86_64.egg
+wget http://downloads.mesosphere.io/master/ubuntu/14.04/$EGG
+# sudo pip install $EGG
